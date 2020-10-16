b9209c036b9d465baa4461e9bd7685a0f50a3ed9
Ariel Alvear <ariel.alvear.l@gmail.com>
2020-10-16 20:33:05 -0300

    desafio finalizado y refactorizado

--- mayor_de_3.rb
+++ mayor_de_3.rb
@@ -1,36 +1,5 @@
-#ver arg1 arg2 y arg3
-#si arg1 es mayor que arg2 y arg3 entonces arg1
-#si arg2 es mayor que arg1 y arg3 entonces arg2
-#si arg3 es mayor que arg1 y arg2 entonces arg3
-
 input1 = ARGV[0]
 input2 = ARGV[1]
 input3 = ARGV[2]
 
-if input1 == input2 || input1 == input3 || input2 == input3
-  if input1 == input2
-    if input1 > input3
-      puts input1
-    else
-      puts input3
-    end
-  elsif input1 == input3
-    if input1 > input2
-      puts input1
-    else
-      puts input2
-    end
-  elsif input2 == input3
-    if input1 > input2
-      puts input1
-    else
-      puts input2
-    end
-  elsif input1 > input2 && input1 > input3
-    puts input1
-  elsif input2 > input1 && input2 > input3
-    puts input2
-  else input3 > input1 && input3 > input2
-    puts input3
-  end
-end
+puts [input1, input2, input3].max
